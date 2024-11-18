48311eb3c0c0406826290d41b1cfa8747299ca84
julietaBdenevi <jburgosdenevi@udesa.edu.ar>
2024-11-18 13:35:46 -0300

    homeScreen

--- TIPARTE2/src/screens/Home.js
+++ TIPARTE2/src/screens/Home.js
@@ -1,19 +1,94 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import {auth} from "../firebase/config"
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList } from 'react-native';
+import {auth, db} from "../firebase/config"
+import Post from "../components/Post"
 
-function HomeMenu(props) {
-   
-    return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Home</Text>
-            <TouchableOpacity></TouchableOpacity>
-            <Text>BIENVENIDO</Text>
+class Home extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          posts: [],
+          loading: true
+        };
+    }
+
+    componentDidMount(){
+        db.collection("posts").onSnapshot(
+            (docs) => {
+                let arrayDocs = []
+                docs.forEach( doc => {
+                    arrayDocs.push({
+                        id: doc.id,
+                        data: doc.data()
+                    })
+                })
+                this.setState({
+                    posts: arrayDocs,
+                    loading: false
+                }, ()=> console.log("posteos en el home: ", JSON.stringify(this.state.posts, null, 4)) )
+            }
+        )
+    }
+
+
+    logout() {
+        auth
+          .signOut()
+          .then(() => {
+            this.props.navigation.navigate("Login");
+          })
+          .catch((error) => console.log(error));
+    }
+
+    render() {
+        return (
+          <View style={styles.container}>
+                <Text>{`Bienvenido ${auth.currentUser.email}`}</Text>
+                <Text style={styles.heading}>Home</Text>  
+                {
+                    this.state.posts.length === 0 ? (
+                        <Text> No hay posts aun</Text>
+                    ) : (
+                    <FlatList 
+                    data={this.state.posts}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => <Post postInfo={item}/>}
+                    />
+                    )
+
+                }
+            <TouchableOpacity style={styles.logoutButton} onPress={() => this.logout()}>
+                <Text style={styles.logoutText}>Logout</Text>
+            </TouchableOpacity>
         </View>
-    );
+        )
+    }
+
+
+   
 }
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#f5f5f5",
+      },
+      heading: {
+        fontSize: 30,
+        fontWeight: "700",
+        marginBottom: 20,
+      },  
+      logoutButton: {
+        backgroundColor: "#ff4444",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+      },
+      logoutText: {
+        color: "white",
+        textAlign: "center",
+      },
 
 });
 
-export default HomeMenu;
+export default Home;
